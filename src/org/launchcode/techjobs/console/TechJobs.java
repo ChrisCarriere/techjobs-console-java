9c334170ec7a03a6041fa8dfac6e1e9a870e0ff6
Chris Carriere <chris.carriere323@gmail.com>
2019-11-12 16:22:08 -0600

    completed part 1 of assignment

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,7 +2,9 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+
 
 /**
  * Created by LaunchCode
@@ -110,7 +112,18 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.isEmpty()) {
+            System.out.println("*****");
+            System.out.println("No results found for specified criteria.");
+            System.out.println("*****");
+        } else {
+            System.out.println("*****");
+            for (HashMap<String, String> job : someJobs) {
+                for (Map.Entry<String, String> field : job.entrySet()) {
+                    System.out.println(field.getKey() + ": " + field.getValue());
+                }
+                System.out.println("*****");
+            }
+        }
     }
 }
